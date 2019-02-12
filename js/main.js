addec5b860b284f8a8b2e44d7c550a2fa8ea8531
Verso <joeymadril@gmail.com>
2019-02-11 16:21:53 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ function getSVAnnounce() {
     xhr.onload = function (e) {
         if (xhr.responseText != "\n") {
             var tmp = JSON.parse(xhr.responseText);
-            document.getElementsByTagName("body")[0].innerHTML = '<div id="announce" style="text-align:center;background-color:#49B9CA;word-wrap:break-word;padding:15px;border-radius:15px;"><h2>' + tmp['title'] + '</h2>' + tmp['content'] + '</div>' + document.getElementsByTagName("body")[0].innerHTML;
+            document.getElementsByTagName("body")[0].innerHTML = '<br><br><br><div id="announce" style="text-align:center;background-color:#ffae00;word-wrap:break-word;padding:15px;"><h2>' + tmp['title'] + '</h2>' + tmp['content'] + '</div>' + document.getElementsByTagName("body")[0].innerHTML;
         }
     }
 }
