e29bc272b556d9b402ac0b5a16ba30288b05599b
Joshua Herron <herronjo110@live.com>
2019-06-10 21:51:49 -0400

    Update user.js

--- js/user.js
+++ js/user.js
@@ -55,18 +55,18 @@ function getStuff(id) {
     } else {
         document.getElementById("followers").innerText = tmp.followers.length + " Followers";
     }
-	document.getElementById("followerstotal").innerHTML = "";
+	document.getElementById("follower").innerHTML = "";
 	for (var i in tmp.followers) {
-		document.getElementById("followerstotal").innerHTML = document.getElementById("followerstotal").innerHTML + '<a href="user.html?id='+tmp.followers[i]+'">'+tmp.followers[i]+'</a><br/><br/>';
+		document.getElementById("follower").innerHTML = document.getElementById("follower").innerHTML + '<div class=" card animated slideInUp"><br/><div class="container"><a href="user.html?id='+tmp.followers[i]+'">'+tmp.followers[i]+'</a></div><br/></div><br/>';
 	}
     if (tmp.following.length == 1) {
         document.getElementById("following").innerText = tmp.following.length + " Following";
     } else {
         document.getElementById("following").innerText = tmp.following.length + " Followings";
     }
-	document.getElementById("followingtotal").innerHTML = "";
+	document.getElementById("fo").innerHTML = "";
 	for (var i in tmp.following) {
-		document.getElementById("followingtotal").innerHTML = document.getElementById("followingtotal").innerHTML + '<a href="user.html?id='+tmp.following[i]+'">'+tmp.following[i]+'</a><br/><br/>';
+		document.getElementById("fo").innerHTML = document.getElementById("fo").innerHTML + + '<div class=" card animated slideInUp"><br/><div class="container"><a href="user.html?id='+tmp.following[i]+'">'+tmp.following[i]+'</a></div><br/></div><br/>';
 	}
     if (localStorage.username != undefined && localStorage.sess != undefined) {
         if (tmp.followers.indexOf(localStorage.username) != -1) {
